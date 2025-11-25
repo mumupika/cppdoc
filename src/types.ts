bd7e4823a9922e8b5215523c7b5f9099fb79011a
Sirui Mu <msrlancern@gmail.com>
2025-11-26 00:42:44 +0800

    feat: add c++ library overview page

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type CppRevision =
+  | "C++98"
+  | "C++11"
+  | "C++14"
+  | "C++17"
+  | "C++20"
+  | "C++23"
+  | "C++26";
+export type CRevision = "C89" | "C99" | "C11" | "C17" | "C23";
+export type CxxRevision = CppRevision | CRevision;
+
+export type DRKind = "cwg" | "lwg";
